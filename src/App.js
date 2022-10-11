90640613d4d0dd0d4b55c3353d4b69bf803b516a
amishari <81028814+amishari@users.noreply.github.com>
2022-10-11 13:27:02 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './components/ExpenseItem.css';
+import ExpenseItem from './components/ExpenseItem';
+
+export default function App() {
+	return (
+		<div className="App">
+			<ExpenseItem />
+			<ExpenseItem />
+			<ExpenseItem />
+		</div>
+	);
+}
