77b5a1c7b9cc45723fce9de3a3cbcd7355bc3ff1
amishari <81028814+amishari@users.noreply.github.com>
2022-10-25 07:36:37 +0330

    Make the code leaner adding a new file, ExpensesList.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -23,19 +23,7 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{filteredExpenses.length === 0 ? (
-					<p> Nothing Found!!</p>
-				) : (
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))
-				)}
-				;
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
